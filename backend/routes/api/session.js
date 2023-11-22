473ce4b5c9622b9d2b73636e1fbc28d6a8b2d6f9
Chase <chase.agee@gmail.com>
2023-11-22 15:21:06 -0800

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -40,4 +40,9 @@ router.post('/', async (req, res, next) => {
   });
 })
 
+router.delete('/', (_req, res) => {
+  res.clearCookie('token');
+  return res.json({ message: 'success' });
+});
+
 module.exports = router;
