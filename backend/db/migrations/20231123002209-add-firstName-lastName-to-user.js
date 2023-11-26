3951de2199a362d9a35452dea3afac7a23033fd0
Chase <chase.agee@gmail.com>
2023-11-26 13:04:33 -0800

    add schema option to user names migration

--- backend/db/migrations/20231123002209-add-firstName-lastName-to-user.js
+++ backend/db/migrations/20231123002209-add-firstName-lastName-to-user.js
@@ -1,6 +1,11 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
