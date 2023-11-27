ba1634419ef646c9f47e99ee00d49fac350619e3
Chase <chase.agee@gmail.com>
2023-11-27 14:41:43 -0800

    added demo group seeder

--- backend/db/models/event.js
+++ backend/db/models/event.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Event.belongsToMany(models.Venue, {
+      Event.belongsTo(models.Venue, {
         foreignKey: 'venueId'
       })
 
